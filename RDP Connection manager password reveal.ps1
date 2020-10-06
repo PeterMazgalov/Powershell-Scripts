8e2791a1596989b19ae401affba8808537e34010
PeterMazgalov <72446045+PeterMazgalov@users.noreply.github.com>
2020-10-06 14:45:22 +0300

    Add files via upload

---
+++ RDP Connection manager password reveal.ps1
@@ -0,0 +1,24 @@
+﻿# Path to RDCMan.exe
+$RDCMan = “C:\RDCMan\RDCMan.exe”
+# Path to RDG file
+$RDGFile = “C:\Users\pmazgalov\OneDrive - KPMG\RDC DB\Cloud Intranet.rdg”
+$TempLocation = “C:\temp”
+
+Copy-Item $RDCMan “$TempLocation\RDCMan.dll”
+Import-Module “$TempLocation\RDCMan.dll”
+$EncryptionSettings = New-Object -TypeName RdcMan.EncryptionSettings
+
+$XML = New-Object -TypeName XML
+$XML.Load($RDGFile)
+$logonCredentials = Select-XML -Xml $XML -XPath ‘//logonCredentials’
+
+$Credentials = New-Object System.Collections.Arraylist
+$logonCredentials | foreach {
+[void]$Credentials.Add([pscustomobject]@{
+Username = $_.Node.userName
+Password = $(Try{[RdcMan.Encryption]::DecryptString($_.Node.password, $EncryptionSettings)}Catch{$_.Exception.InnerException.Message})
+Domain = $_.Node.domain
+})
+} | Sort Username
+
+$Credentials | Sort Username
